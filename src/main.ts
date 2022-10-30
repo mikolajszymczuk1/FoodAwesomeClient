a8222af03962339b9e1f92e472bcd922db4cd0fc
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-10-30 17:19:55 +0100

    Created first routing logic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
-import './scss/main.scss';
+import '@/scss/main.scss';
+// @ts-ignore
+import Router from '@/modules/Router/Router.ts';
+// @ts-ignore
+import Page from '@/modules/Page/Page.ts';
 
+// Get root app element
 const app = document.querySelector('#app') as HTMLElement;
-const div = document.createElement('div') as HTMLDivElement;
 
-div.classList.add('hello');
-div.innerHTML = 'Hello World !';
+// Router;
+let router: Router;
 
-app.appendChild(div);
+// Pages
+let mainPage: Page;
+
+// Create pages objects
+const loadAllPages = async () => {
+  mainPage = new Page('mainPage', await Page.loadSinglePage('templates/mainPage.mustache.html'));
+};
+
+// Init function
+const init = async () => {
+  await loadAllPages();
+  router = new Router([
+    { route: '/', page: mainPage },
+  ], app);
+
+  router.setPage('/');
+};
+
+init();
---
+++ src/modules/Router/Router.ts
@@ -0,0 +1,41 @@
+// @ts-ignore
+import { Route } from './types.ts';
+
+export default class Router {
+  private routes: Object[];
+
+  private appRootElement: HTMLElement;
+
+  constructor(routesConfig: Object[], appRootElement: HTMLElement) {
+    this.routes = routesConfig;
+    this.appRootElement = appRootElement;
+  }
+
+  /**
+   * Get all routes
+   * @returns {Object[]} Array of routes objects
+   */
+  getRoutes(): Object[] {
+    return this.routes;
+  }
+
+  /**
+   * Get route object with specific route path
+   * @param {string} routePath
+   * @returns {Route} single route object
+   */
+  getTemplate(routePath: string): Route {
+    const routeObj = this.routes.find((el: Route) => el.route === routePath) as Route;
+    return routeObj;
+  }
+
+  /**
+   * Set new current page
+   * @param {string} routePath route path eg. '/'
+   * @param {Object} data extra data for template
+   */
+  public setPage(routePath: string, data: Object = { }): void {
+    const renderedPage = this.getTemplate(routePath).page.render(data);
+    this.appRootElement.innerHTML = renderedPage;
+  }
+}
