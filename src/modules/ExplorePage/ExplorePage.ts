3c896886a50c1a5953757334e7dddd57cc47c83f
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-01-02 14:20:55 +0100

    Created hero section for explore page

--- src/modules/ExplorePage/ExplorePage.ts
+++ src/modules/ExplorePage/ExplorePage.ts
@@ -1,9 +1,13 @@
 // @ts-ignore
 import Page from '@/modules/Page/Page.ts';
+// @ts-ignore
+import Navigation from '@/modules/Navigation/Navigation.ts';
 
 export default class ExplorePage extends Page {
   // eslint-disable-next-line class-methods-use-this
   public init(): void {
-    console.log('Explore Page');
+    // Init Navigation
+    const navigation = new Navigation();
+    navigation.init();
   }
 }
