0ebced9c5661f9166427ce595465a617a0ca16d3
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-01-02 15:29:26 +0100

    Start working on last added section in explore page

--- src/modules/ExplorePage/ExplorePage.ts
+++ src/modules/ExplorePage/ExplorePage.ts
@@ -1,13 +1,57 @@
 // @ts-ignore
 import Page from '@/modules/Page/Page.ts';
 // @ts-ignore
-import Navigation from '@/modules/Navigation/Navigation.ts';
+import NavigationBar from '@/modules/Navigation/Navigation.ts';
+import Swiper, { Navigation, A11y } from 'swiper';
+
+import 'swiper/scss';
 
 export default class ExplorePage extends Page {
+  readonly swiperSelector: string = '.exploreLastAddedSection__swiper';
+
+  readonly swiperNavButtonPrevSelector: string = '.exploreLastAddedSection__navButton--prev';
+
+  readonly swiperNavButtonNextSelector: string = '.exploreLastAddedSection__navButton--next';
+
   // eslint-disable-next-line class-methods-use-this
   public init(): void {
     // Init Navigation
-    const navigation = new Navigation();
+    const navigation = new NavigationBar();
     navigation.init();
+
+    // Setup Swiper slider
+    this.setupSwiper();
+  }
+
+  /**
+   * Create Swiper instance
+   */
+  private setupSwiper(): void {
+    // eslint-disable-next-line no-unused-vars
+    const swiperObj = new Swiper(this.swiperSelector, {
+      modules: [Navigation, A11y],
+      direction: 'horizontal',
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 15,
+      loop: true,
+      a11y: {
+        prevSlideMessage: 'Previous slide',
+        nextSlideMessage: 'Next slide',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      navigation: {
+        nextEl: this.swiperNavButtonNextSelector,
+        prevEl: this.swiperNavButtonPrevSelector,
+      },
+      breakpoints: {
+        768: {
+          spaceBetween: 30,
+        },
+      },
+    });
   }
 }
